1191145c31d8cdf82d4be917efb7da4e26d28905
KiryaP <kirill19962003@gmail.com>
2022-03-09 20:00:14 +0300

    add lab

---
+++ Programming/lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _153501_Pryhozhy
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter first number: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            int result = first / second;
+            Console.WriteLine($"{first} / {second} = {result}");
+        }
+    }
+}
