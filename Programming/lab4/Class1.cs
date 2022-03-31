4195ec4c59bd33098284e5bbcdea59c8d6f170b1
KiryaP <kirill19962003@gmail.com>
2022-03-31 13:54:37 +0300

    add lab4

---
+++ Programming/lab4/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Ticket
+    {
+        private int cost;
+        private string path;
+
+
+        public Ticket(string Path = "Волковыск")
+        {
+            cost = 100;
+            path = Path;
+        }
+
+        public int Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+    }
+
+    public class Station
+    {
+        private static Ticket ticket;
+        private string nameStation;
+        private int saledTicketsCount;
+        private int seatsCount;
+        private static Station StationInstance;
+
+        private Station()
+        {
+            nameStation = "Минск";
+            saledTicketsCount = 5;
+            seatsCount = 10;
+        }
+
+        public Ticket Ticket
+        {
+            get { return ticket; }  
+
+            set { ticket = value; }
+        }
+
+        public int SaledTicketsCount
+        {
+            get { return saledTicketsCount; }
+
+            set {  saledTicketsCount = value; }
+        }
+
+        public int SeatsCount
+        {
+            get { return seatsCount; }
+
+            set { seatsCount = value; }
+        }
+
+        public string NameStation
+        {
+            get { return nameStation; } 
+
+            set { nameStation = value; }    
+        }
+
+        public static Station GetInstanceStation()
+        {
+            if(StationInstance == null)
+            {
+                StationInstance = new Station();
+                ticket = new Ticket();
+            }  
+            return StationInstance;
+        }
+
+        public int CalcCostNotSaledTickets()
+        {
+            return (seatsCount - saledTicketsCount) * ticket.Cost;
+        }
+
+
+
+    }
+}
---
+++ Programming/lab4/StationTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.Tests
+{
+    [TestClass]
+    public class StationTests
+    {
+        [TestMethod()]
+        public void CalcCostNotSaledTicketsTest12_200_100()
+        {
+            int saledTickets = 12;
+            int seatsCount = 200;
+            int costOfTicket = 100;
+
+            Station.GetInstanceStation().SaledTicketsCount = saledTickets;
+            Station.GetInstanceStation().SeatsCount = seatsCount;
+            Station.GetInstanceStation().Ticket.Cost = costOfTicket;
+
+            int expected = (seatsCount - saledTickets) * costOfTicket;
+
+            Assert.AreEqual(expected, Station.GetInstanceStation().CalcCostNotSaledTickets());
+        }
+        [TestMethod()]
+        public void CalcCostNotSaledTicketsTest20_120_230()
+        {
+            int saledTickets = 20;
+            int seatsCount = 120;
+            int costOfTicket = 230;
+
+            Station.GetInstanceStation().SaledTicketsCount = saledTickets;
+            Station.GetInstanceStation().SeatsCount = seatsCount;
+            Station.GetInstanceStation().Ticket.Cost = costOfTicket;
+
+            int expected = (seatsCount - saledTickets) * costOfTicket;
+
+            Assert.AreEqual(expected, Station.GetInstanceStation().CalcCostNotSaledTickets());
+        }
+
+        [TestMethod()]
+        public void CalcCostNotSaledTicketsTest44_45_100000()
+        {
+            int saledTickets = 44;
+            int seatsCount = 45;
+            int costOfTicket = 100000;
+
+            Station.GetInstanceStation().SaledTicketsCount = saledTickets;
+            Station.GetInstanceStation().SeatsCount = seatsCount;
+            Station.GetInstanceStation().Ticket.Cost = costOfTicket;
+
+            int expected = (seatsCount - saledTickets) * costOfTicket;
+
+            Assert.AreEqual(expected, Station.GetInstanceStation().CalcCostNotSaledTickets());
+        }
+
+        [TestMethod()]
+        public void ChangeCostOfTicketTest12345()
+        {
+            int expected = 12345;
+            Station.GetInstanceStation().Ticket.Cost = expected;
+
+            Assert.AreEqual(expected, Station.GetInstanceStation().Ticket.Cost);
+        }
+
+        [TestMethod()]
+        public void ChangeCostOfTicketTest3333()
+        {
+            int expected = 3333;
+            Station.GetInstanceStation().Ticket.Cost = expected;
+
+            Assert.AreEqual(expected, Station.GetInstanceStation().Ticket.Cost);
+        }
+
+        [TestMethod()]
+        public void ChangeCostOfTicketTest1()
+        {
+            int expected = 1;
+            Station.GetInstanceStation().Ticket.Cost = expected;
+
+            Assert.AreEqual(expected, Station.GetInstanceStation().Ticket.Cost);
+        }
+
+
+    }
+}
