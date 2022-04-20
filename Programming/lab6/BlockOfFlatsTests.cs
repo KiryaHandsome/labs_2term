59a014ac97f70d059ba4d247a94b2b9c642ab8a7
KiryaP <kirill19962003@gmail.com>
2022-04-20 18:09:59 +0300

    add tests

---
+++ Programming/lab6/BlockOfFlatsTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Buildings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buildings.Tests
+{
+    [TestClass()]
+    public class BlockOfFlatsTests
+    {
+        [TestMethod()]
+        public void GetMaterialTest()
+        {
+            BlockOfFlats block = new BlockOfFlats(1, 1, "rect");
+            string expected = "Foam Block";
+            Assert.AreEqual(expected, block.GetMaterial());
+        }
+
+        [TestMethod()]
+        public void NumOfFlatsProperty_100_Test()
+        {
+            BlockOfFlats block = new BlockOfFlats(100, 9, "shape");
+            int expected = 100;
+            Assert.AreEqual(expected, block.NumberOfFlats);
+        }
+
+        [TestMethod()]
+        public void GetDebtForElectricityStartTest()
+        {
+            BlockOfFlats block = new BlockOfFlats(0, 0, "shape");
+            int expected = 100;
+            Assert.AreEqual(expected, block.GetDebtForElectricity());
+        }
+
+        [TestMethod()]
+        public void GetDebtForElectricityPay50Test()
+        {
+            BlockOfFlats block = new BlockOfFlats(0, 0, "shape");
+            int expected = 50;
+            block.PayForElectricity(50);
+            Assert.AreEqual(expected, block.GetDebtForElectricity());
+        }
+
+        public void GetDebtForElectricityPay150Test()
+        {
+            BlockOfFlats block = new BlockOfFlats(0, 0, "shape");
+            int expected = -50;
+            block.PayForElectricity(150);
+            Assert.AreEqual(expected, block.GetDebtForElectricity());
+        }
+
+        [TestMethod()]
+        public void PayForElectricity30Test()
+        {
+            BlockOfFlats block = new BlockOfFlats(0, 0, "shape");
+            int expected = 70;
+            block.PayForElectricity(30);
+            Assert.AreEqual(expected, block.GetDebtForElectricity());
+        }
+
+        [TestMethod()]
+        public void ShapeRectangleTest()
+        {
+            BlockOfFlats block = new BlockOfFlats(0, 0, "Rectangle");
+            string expected = "Rectangle";
+            Assert.AreEqual(expected, block.Shape);
+        }
+
+        [TestMethod()]
+        public void ShapeCircleTest()
+        {
+            BlockOfFlats block = new BlockOfFlats(0, 0, "Circle");
+            string expected = "Circle";
+            Assert.AreEqual(expected, block.Shape);
+        }
+
+        [TestMethod()]
+        public void ShapeRhombTest()
+        {
+            BlockOfFlats block = new BlockOfFlats(0, 0, "Rhomb");
+            string expected = "Rhomb";
+            Assert.AreEqual(expected, block.Shape);
+        }
+    }
+}
---
+++ Programming/lab6/FactoryTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Buildings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buildings.Tests
+{
+    [TestClass()]
+    public class FactoryTests
+    {
+        [TestMethod()]
+        public void EmployesAmountProperty1000Test()
+        {
+            Factory factory = new Factory("Pizza", 1000, 40, "Rectangle");
+            int expected = 1000;
+            Assert.AreEqual(expected, factory.EmployesAmount);
+        }
+
+        [TestMethod()]
+        public void EmployesAmountProperty1Test()
+        {
+            Factory factory = new Factory("Pizza", 1, 40, "Rectangle");
+            int expected = 1;
+            Assert.AreEqual(expected, factory.EmployesAmount);
+        }
+
+        [TestMethod()]
+        public void EmployesAmountProperty99Test()
+        {
+            Factory factory = new Factory("Pizza", 99, 40, "Rectangle");
+            int expected = 99;
+            Assert.AreEqual(expected, factory.EmployesAmount);
+        }
+
+        [TestMethod()]
+        public void GetMaterialTest()
+        {
+            Factory factory = new Factory("Pizza", 99, 40, "Rectangle");
+            string expected = "Brick";
+            Assert.AreEqual(expected, factory.GetMaterial());
+        }
+
+        [TestMethod()]
+        public void ShapeRectangleTest()
+        {
+            Factory factory = new Factory("Pizza", 99, 40, "Rectangle");
+            string expected = "Rectangle";
+            Assert.AreEqual(expected, factory.Shape);
+        }
+
+        [TestMethod()]
+        public void ShapeCircleTest()
+        {
+            Factory factory = new Factory("Pizza", 99, 40, "Circle");
+            string expected = "Circle";
+            Assert.AreEqual(expected, factory.Shape);
+        }
+
+        [TestMethod()]
+        public void ShapeRhombTest()
+        {
+            Factory factory = new Factory("Pizza", 99, 40, "Rhomb");
+            string expected = "Rhomb";
+            Assert.AreEqual(expected, factory.Shape);
+        }
+    }
+}
